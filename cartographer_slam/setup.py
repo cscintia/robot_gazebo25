23fb98609dffc4d148639b5293c98dcba97ddebf
cscintia <csermakcintia14@gmail.com>
2025-12-02 21:42:58 +0100

    fix config path in setup.py

--- cartographer_slam/setup.py
+++ cartographer_slam/setup.py
@@ -11,8 +11,7 @@ setup(
     data_files=[
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
-        (os.path.join('share', package_name), glob('config/*.yaml')),
-        (os.path.join('share', package_name), glob('config/*.lua')),         
+        (os.path.join('share', package_name, 'config'), glob('config/*')),      
     ],
     install_requires=['setuptools'],
     zip_safe=True,
